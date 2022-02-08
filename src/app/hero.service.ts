be5789073ceb922bf4f92344f4deb4705481eb3d
hoaiphu99 <hoaiphu.ptit@gmail.com>
2022-02-08 11:55:11 +0700

    3 files modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -22,19 +22,32 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     // const heroes = of(HEROES);
     // this.messageService.add('HeroService: fetched heroes');
-    return this.http
-      .get<Hero[]>(this.heroesUrl)
-      .pipe(catchError(this.handleError<Helo[]>('getHeroes', [])));
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes')),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   getHero(id: number): Observable<Hero> {
-    const hero = HEROES.find((h) => h.id === id)!;
-    this.messageService.add(`HeroesService: fetched hero id = ${id}`);
+    // const hero = HEROES.find((h) => h.id === id)!;
+    // this.messageService.add(`HeroesService: fetched hero id = ${id}`);
+    const url = `${this.heroesUrl}/${id}`;
 
-    return of(hero);
+    return this.http.get<Hero>(url).pipe(
+      tap((_) => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      this.log(`${operation} failed: ${error.message}`);
+
+      return of(result as T);
+    };
   }
 }
